ed94b163eb57c296cabb073a49ba9d53169121d0
taigarowe <70992313+taigarowe@users.noreply.github.com>
2021-02-02 17:56:48 -0600

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,18 @@
+class Dustbin {
+    constructor(x, y, width, height){
+        var options = {
+            'isStatic' : true
+        }
+        this.width = width;
+        this.height = height;
+        this.bodyb = Bodies.rectangle(x,y,width,height,options);    
+        World.add(world, this.bodyb);
+    }
+
+    display (){
+     var pos = this.bodyb.position;
+     //rectMode(CENTER);
+     rect(pos.x,pos.y,this.width,this.height);
+    }
+}
+
